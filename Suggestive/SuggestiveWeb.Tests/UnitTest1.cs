a350f30803d19b65b226701d2b01b17a928461a8
Steve4th <steveforth@hotmail.com>
2015-04-16 20:33:35 +0100

    Added XUnit as the testing framework

---
+++ Suggestive/SuggestiveWeb.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SuggestiveWeb.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestOne()
+        {
+            Assert.True(false, "check to see if xunit runner is working");
+        }
+    }
+}
