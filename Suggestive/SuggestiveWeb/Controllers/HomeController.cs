6011ff843848005efcb3a10dd8f8f9c0f36981b7
Steve4th <steveforth@hotmail.com>
2015-04-16 20:45:14 +0100

    Removed KnockoutJs from the web site

--- Suggestive/SuggestiveWeb/Controllers/HomeController.cs
+++ Suggestive/SuggestiveWeb/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Web.Mvc;
 
 namespace SuggestiveWeb.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
