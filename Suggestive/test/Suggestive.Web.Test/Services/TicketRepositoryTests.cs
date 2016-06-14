4695bd7be93f9a1c37233f21abd798af74566d7a
Steve 4th <steveforth@hotmail.com>
2016-06-14 11:55:55 +0100

    Added test class for ticket repo

---
+++ Suggestive/test/Suggestive.Web.Test/Services/TicketRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Suggestive.Web.Services;
+using Xunit;
+
+namespace Suggestive.Web.Test.Services
+{
+    public class TicketRepositoryTests
+    {
+        [Fact]
+        public void GetAllTickets_ReturnsTickets()
+        {
+            ITicketRepository repo = new TicketRepository();
+            var tickets = repo.GetAllTickets();
+            Assert.True(tickets.Any(), "Expected 1 or more tickets returned");
+        }
+    }
+}
