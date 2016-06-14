7ebca5160c14f3737c20ce17a9f72305e56b1833
Steve 4th <steveforth@hotmail.com>
2016-06-14 09:40:10 +0100

    Added reference to the suggestive.web project and created a test to check the reference is working

---
+++ Suggestive/test/Suggestive.Web.Test/TicketStatusViewModelTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Suggestive.Web.Models.Requirements;
+using Suggestive.Web.ViewModels.Requirements;
+using Xunit;
+
+namespace suggestive.web.test
+{
+    public class TicketStatusViewModelTests
+    {
+        [Fact]
+        public void ToDoTickets_OneOrMoreToDoTickets_ExpectPropertyToContainsRecords()
+        {
+            var ticketStatusViewModel = new TicketStatusViewModel()
+            {
+                AllTickets = new List<Ticket>()
+                {
+                    new Ticket() { Summary = "A requirement", Status = TicketStatus.ToDo },
+                    new Ticket() { Summary = "Requirement Two", Status = TicketStatus.ToDo },
+                    new Ticket() { Summary = "Requirement Three", Status = TicketStatus.InProgress },
+                    new Ticket() { Summary = "Requirement Four", Status = TicketStatus.Complete },
+                    new Ticket() { Summary = "Requirement Five", Status = TicketStatus.Rejected }
+                }
+            };
+            var expectedNumberOfTodoItems = 2; 
+
+            Assert.Equal(expectedNumberOfTodoItems, ticketStatusViewModel.ToDoTickets.Count());
+        }
+    }
+}
