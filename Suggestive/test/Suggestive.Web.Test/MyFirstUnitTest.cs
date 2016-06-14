cc04208a4e0cea2e13b82a37f2a3b3b96e80e14c
Steve 4th <steveforth@hotmail.com>
2016-06-14 09:21:43 +0100

    Added first tests to test out framework & runners

---
+++ Suggestive/test/Suggestive.Web.Test/MyFirstUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace suggestive.web.test
+{
+    public class MyFirstUnitTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(42, 6*7);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(42, 5 * 17);
+        }
+    }
+}
