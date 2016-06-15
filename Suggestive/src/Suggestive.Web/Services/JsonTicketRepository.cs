7e184c8a3a420af0a776f5c596e774ce4f5ed98e
Steve 4th <steveforth@hotmail.com>
2016-06-15 16:15:28 +0100

    Created a ticket repo that reads from a Json file

---
+++ Suggestive/src/Suggestive.Web/Services/JsonTicketRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Suggestive.Web.Models.Requirements;
+
+namespace Suggestive.Web.Services
+{
+    public class JsonTicketRepository : ITicketRepository
+    {
+        public async Task<IEnumerable<Ticket>> GetAllTicketsAsync()
+        {
+            return await Task.FromResult(this.GetAllTicketsFromJsonFile());
+        }
+
+        private IEnumerable<Ticket> GetAllTicketsFromJsonFile()
+        {
+            var jsonFilePath = "RequirementTickets.json";
+            using (var textReader = System.IO.File.OpenText(jsonFilePath))
+            {
+                var reader = new JsonTextReader(textReader);
+                var jsonDeseriliser = new JsonSerializer();
+                var tickets = jsonDeseriliser.Deserialize<List<Ticket>>(reader);
+                return tickets.AsEnumerable();
+            }
+        }
+
+    }
+}
---
+++ Suggestive/test/Suggestive.Web.Test/Services/JsonTicketRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Suggestive.Web.Services;
+using Xunit;
+
+namespace Suggestive.Web.Test.Services
+{
+    public class JsonTicketRepositoryTests
+    {
+        [Fact]
+        public async Task GetAllTickets_ReturnsTickets()
+        {
+            ITicketRepository repo = new JsonTicketRepository();
+            var tickets = await repo.GetAllTicketsAsync();
+            Assert.True(tickets.Any(), "Expected 1 or more tickets returned");
+        }
+    }
+}
