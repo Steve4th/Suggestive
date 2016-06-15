44ab5fa8f352ad73d33e6cbc2d554d98bd1955fd
Steve 4th <steveforth@hotmail.com>
2016-06-15 14:28:37 +0100

    Created a helper to obtain the display name for enum values

---
+++ Suggestive/src/Suggestive.Web/Models/Requirements/EnumDisplayNameExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace Suggestive.Web.Models.Requirements
+{
+    public static class EnumDisplayNameExtension
+    {
+        public static string GetDisplayName<T>(this T value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+            var displayAttributes = fieldInfo.GetCustomAttributes<DisplayAttribute>();
+
+            return displayAttributes.Any() ? displayAttributes.First().Name : value.ToString();
+        }
+    }
+}
---
+++ Suggestive/test/Suggestive.Web.Test/EnumDisplayNameExtensionTests.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Suggestive.Web.Models.Requirements;
+using Xunit;
+
+namespace Suggestive.Web.Test
+{
+    public class EnumDisplayNameExtensionTests
+    {
+        [Fact]
+        public void GetDisplayName_EnumWithDisplayAttribute_ExpectDisplayAttributeValue()
+        {
+            var expectedDisplayName = "This Enum Value Has a Display Name";
+            var result = GetDisplayNameTestEnum.EnumValueWithDisplayName.GetDisplayName();
+            Assert.Equal(expectedDisplayName, result);
+        }
+
+        [Fact]
+        public void GetDisplayName_EnumWithoutDisplayAttribute_ExpectEnumItemName()
+        {
+            var expectedDisplayName = "EnumValueWithoutDisplayName";
+            var result = GetDisplayNameTestEnum.EnumValueWithoutDisplayName.GetDisplayName();
+            Assert.Equal(expectedDisplayName, result);
+        }
+    }
+
+    internal enum GetDisplayNameTestEnum
+    {
+        [Display(Name="This Enum Value Has a Display Name")]
+        EnumValueWithDisplayName = 0,
+        EnumValueWithoutDisplayName = 1
+    }
+}
