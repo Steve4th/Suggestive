988070b98d29c817f4ab640aa812402ca9a03af2
Steve 4th <steveforth@hotmail.com>
2016-06-15 15:11:55 +0100

    Added display name for Ticket Status enum

--- Suggestive/src/Suggestive.Web/Models/Requirements/TicketStatus.cs
+++ Suggestive/src/Suggestive.Web/Models/Requirements/TicketStatus.cs
@@ -1,10 +1,16 @@
-﻿namespace Suggestive.Web.Models.Requirements
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Suggestive.Web.Models.Requirements
 {
     public enum TicketStatus
     {
+        [Display(Name = "Not Started")]
         ToDo = 0,
+        [Display(Name="In Progress")]
         InProgress = 1,
+        [Display(Name = "Completed")]
         Complete = 2,
+        [Display(Name = "Rejected")]
         Rejected = 3
     }
 }
