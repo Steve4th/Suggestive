5ccbe8593f0432220a14243d62729c57d6740e4a
Steve 4th <steveforth@hotmail.com>
2016-06-16 15:10:04 +0100

    Added stub WebApi controller for requirements

---
+++ Suggestive/src/Suggestive.Web/Api/RequirementController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Suggestive.Web.Models.Requirements;
+
+namespace Suggestive.Web.Api
+{
+    [Route("api/[controller]")]
+    public class RequirementController: Controller
+    {
+        [HttpGet("{id:int}")]
+        public Ticket Get(int id)
+        {
+            return new Ticket()
+            {
+                Description = "With this ticket you can access Wonka's chocolate factory",
+                Summary = $"Golden Ticket #{id}",
+                Status = TicketStatus.Complete
+            };
+        }
+    }
+}
