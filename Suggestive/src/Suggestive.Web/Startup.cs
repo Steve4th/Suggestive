71dde7b0ffd1760767e9664d78c2245c2493a95f
Steve 4th <steveforth@hotmail.com>
2016-06-16 13:51:25 +0100

    Updated DI to inject file path for ticket repro

--- Suggestive/src/Suggestive.Web/Startup.cs
+++ Suggestive/src/Suggestive.Web/Startup.cs
@@ -53,7 +53,8 @@ namespace Suggestive.Web
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
-            services.AddSingleton<ITicketRepository, JsonTicketRepository>();
+            var ticketsJsonFilePath = "RequirementTickets.json";
+            services.AddSingleton<ITicketRepository>(new JsonTicketRepository(ticketsJsonFilePath));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
