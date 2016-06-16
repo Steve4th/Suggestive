3026cc612d2eaac01fffa9e67f8d2c016227b90e
Steve 4th <steveforth@hotmail.com>
2016-06-16 15:26:02 +0100

    Updated requirements api controller - add async support and ticket repo dependency

--- Suggestive/src/Suggestive.Web/Api/RequirementController.cs
+++ Suggestive/src/Suggestive.Web/Api/RequirementController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Suggestive.Web.Models.Requirements;
+using Suggestive.Web.Services;
 
 namespace Suggestive.Web.Api
 {
     [Route("api/[controller]")]
     public class RequirementController: Controller
     {
+        private readonly ITicketRepository _ticketRepository;
+
+        public RequirementController(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
         [HttpGet("{id:int}")]
-        public Ticket Get(int id)
+        public async Task<Ticket> Get(int id)
         {
             return new Ticket()
             {
