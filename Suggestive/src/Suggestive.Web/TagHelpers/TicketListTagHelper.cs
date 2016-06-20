babcd01369f7a39157f2dba51a7fd9d21c96f4e3
Steve 4th <steveforth@hotmail.com>
2016-06-20 14:50:23 +0100

    Created tag helper to display requirement tickets

---
+++ Suggestive/src/Suggestive.Web/TagHelpers/TicketListTagHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Suggestive.Web.Models.Requirements;
+
+namespace Suggestive.Web.TagHelpers
+{
+    public class TicketListTagHelper: TagHelper
+    {
+        public IEnumerable<Ticket> Tickets { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (Tickets == null)
+            {
+                throw new InvalidOperationException($"{nameof(this.Tickets)} must be specified");
+            }
+
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            TagBuilder listGroupContainerBuilder = new TagBuilder("div");
+            listGroupContainerBuilder.AddCssClass("list-group");
+
+            foreach (var ticket in this.Tickets)
+            {
+                TagBuilder itemBuilder = new TagBuilder("a");
+                itemBuilder.AddCssClass("list-group-item");
+                itemBuilder.Attributes.Add("href", "#");
+
+                TagBuilder headingBuilder = new TagBuilder("h4");
+                headingBuilder.AddCssClass("list-group-item-heading");
+                headingBuilder.InnerHtml.AppendHtml(
+                    $"<span class=\"fa fa-puzzle-piece fa-lg\" aria-hidden=\"true\"></span>{ticket.Summary}");
+
+                TagBuilder badgeBuilder = new TagBuilder("span");
+                badgeBuilder.AddCssClass("badge");
+                badgeBuilder.InnerHtml.Append(ticket.Id.ToString());
+
+                TagBuilder descriptionBuilder = new TagBuilder("p");
+                descriptionBuilder.AddCssClass("list-group-item-text");
+                descriptionBuilder.InnerHtml.Append(ticket.Description);
+
+                itemBuilder.InnerHtml.AppendHtml(headingBuilder);
+                itemBuilder.InnerHtml.AppendHtml(badgeBuilder);
+                itemBuilder.InnerHtml.AppendHtml(descriptionBuilder);
+
+                listGroupContainerBuilder.InnerHtml.AppendHtml(itemBuilder);
+            }
+            output.Content.AppendHtml(listGroupContainerBuilder);
+        }
+    }
+}
---
+++ Suggestive/test/Suggestive.Web.Test/TagHelpers/TicketListTagHelperTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Suggestive.Web.Models.Requirements;
+using Suggestive.Web.TagHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Suggestive.Web.Test.TagHelpers
+{
+    public class TicketListTagHelperTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public TicketListTagHelperTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void Process_EmptyTicketListSupplied_ExpectDivWithNoListItems()
+        {
+            var tickets = new List<Ticket>();
+            var expectedHtmlContent = @"<div class=""list-group""></div>";
+
+            Process_GeneratedExpectedOutput(tickets, expectedHtmlContent);
+        }
+
+        [Fact]
+        public void Process_PopulatedTicketList_ExpectDivWithListItems()
+        {
+            var tickets = new List<Ticket>()
+            {
+                new Ticket() { Id= 1, Summary = "Summary One", Description = "Description One"},
+                new Ticket() { Id= 2, Summary = "Summary Two", Description = "Description Two"},
+            };
+            var expectedHtmlContent = 
+            @"<div class=""list-group"">
+                    <a class=""list-group-item"" href=""#"">
+                            <h4 class=""list-group-item-heading"">
+                            <span class=""fa fa-puzzle-piece fa-lg"" aria-hidden=""true""></span>Summary One</h4>
+                            <span class=""badge"">1</span>
+                            <p class=""list-group-item-text"">Description One</p>
+                    </a>
+                    <a class=""list-group-item"" href=""#"">
+                            <h4 class=""list-group-item-heading"">
+                            <span class=""fa fa-puzzle-piece fa-lg"" aria-hidden=""true""></span>Summary Two</h4>
+                            <span class=""badge"">2</span>
+                            <p class=""list-group-item-text"">Description Two</p>
+                    </a>
+            </div>"
+            .Replace("  ","")
+            .Replace(Environment.NewLine, "");
+
+            Process_GeneratedExpectedOutput(tickets, expectedHtmlContent);
+        }
+
+        [Fact]
+        public void Process_NullTicketList_ExpectException()
+        {
+            IEnumerable<Ticket> tickets = null;
+            var expectedHtmlContent = "<div></div>";
+
+            Assert.Throws<InvalidOperationException>(() => Process_GeneratedExpectedOutput(tickets, expectedHtmlContent));
+        }
+
+        private void Process_GeneratedExpectedOutput(IEnumerable<Ticket> tickets, string expectedHtmlContent)
+        {
+            var tagHelper = new TicketListTagHelper {Tickets = tickets};
+
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+
+            var tagHelperOutput = new TagHelperOutput("ticket-list",
+                new TagHelperAttributeList(),
+                (htmlEncoder, helperContextTask) =>
+                {
+                    var tagHelperContent = new DefaultTagHelperContent();
+                    tagHelperContent.SetContent(string.Empty);
+                    return Task.FromResult<TagHelperContent>(tagHelperContent);
+                }
+                );
+            tagHelper.Process(tagHelperContext, tagHelperOutput);
+
+            _outputHelper.WriteLine("Tag Helper Process completed. Content generated is :");
+            _outputHelper.WriteLine(tagHelperOutput.Content.GetContent());
+
+            _outputHelper.WriteLine("");
+            _outputHelper.WriteLine("Expected tag helper output is:");
+            _outputHelper.WriteLine(expectedHtmlContent);
+
+            Assert.Equal("div", tagHelperOutput.TagName);
+            Assert.Equal(expectedHtmlContent, tagHelperOutput.Content.GetContent());
+        }
+
+
+
+    }
+}
