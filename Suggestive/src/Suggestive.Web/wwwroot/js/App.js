0e06eed1eaeb2e4b289e67b52fc28ad4421d3baa
Steve 4th <steveforth@hotmail.com>
2016-06-22 15:48:46 +0100

    initial angular controller added

--- Suggestive/src/Suggestive.Web/wwwroot/js/App.js
+++ Suggestive/src/Suggestive.Web/wwwroot/js/App.js
@@ -1,3 +1,17 @@
-﻿(function() {
+﻿(function () {
+    "use strict";
+
     angular.module("suggestive", []);
+
+    angular.module("suggestive")
+        .controller("suggestionController", suggestionController)
+
+    function suggestionController() {
+
+        var vm = this;
+
+        vm.name = "suggestion please";
+
+    }
+
 })();
