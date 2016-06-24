83ed11ac8a70bac31ed6a888ebf6bf3738a78c3c
Steve 4th <steveforth@hotmail.com>
2016-06-24 09:28:59 +0100

    clean up javascript formatting

--- Suggestive/src/Suggestive.Web/wwwroot/js/App.js
+++ Suggestive/src/Suggestive.Web/wwwroot/js/App.js
@@ -22,22 +22,16 @@
             vm.errorMessage = "";
 
             vm.saveSuggestion();
-            //vm.suggestions.push({
-            //    title: vm.newSuggestion.title,
-            //    createdOn: new Date(),
-            //    description: ""
-            //});
+
             vm.newSuggestion = {};
             vm.isBusy = false;
         }
 
         $http.get("/api/suggestions")
             .then(function (response) {
-                //success
                 angular.copy(response.data, vm.suggestions)
             },
             function (error) {
-                //failure
                 vm.errorMessage = "Problem getting suggestions: " + error.status + " - " + error.statusText;
             })
         .finally(function () {
@@ -48,7 +42,6 @@
             $http.post("/api/suggestions/", vm.newSuggestion)
                 .then(function (reponse) {
                     vm.suggestions.push(response.data);
-
                 }, function (error) {
                     vm.errorMessage = "Failed to save suggestion: " + error.status + " - " + error.statusText;
                 });
