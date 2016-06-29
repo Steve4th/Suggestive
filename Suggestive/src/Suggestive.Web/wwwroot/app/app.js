a5d83d3a3024cc486ee14126b65f96ab884020dd
Steve 4th <steveforth@hotmail.com>
2016-06-29 16:04:23 +0100

    extracted editor controller to it's own script file

--- Suggestive/src/Suggestive.Web/wwwroot/app/app.js
+++ Suggestive/src/Suggestive.Web/wwwroot/app/app.js
@@ -65,43 +65,6 @@
         }
     }
 
-//Suggestive.Controllers.Suggestive
-    angular.module("app")
-        .controller("suggestionEditorController", suggestionEditorController);
-
-    function suggestionEditorController($routeParams, $http) {
-        var vm = this;
-
-        vm.errorMessage = "";
-        vm.statusMessage = "";
-        vm.isBusy = true;
-        vm.suggestionId = $routeParams.suggestionId;
-        vm.suggestion = {};
-        
-        vm.getSuggestion = function () {
-            $http.get("/api/suggestions/" + vm.suggestionId)
-                .then(function(response) {
-                    angular.copy(response.data, vm.suggestion)
-                }, 
-                function(error) {
-                    vm.errorMessage = "Problem getting suggestion: " + error.status + " - " + error.statusText;
-                })
-                .finally(function() {
-                    vm.isBusy = false;
-                });
-        }
-        vm.getSuggestion();
-
-        vm.updateSuggestion = function () {
-            $http.put("/api/suggestions/"  + vm.suggestionId, vm.suggestion)
-                .then(function (response) {
-                    vm.statusMessage = "Suggestion updated successfully";
-                }, function (error) {
-                    vm.errorMessage = "Failed to save suggestion: " + error.status + " - " + error.statusText;
-                });
-        }
-    }
-
 // Suggestive.Controls.WaitCursor
     angular.module("controls", [])
             .directive("waitCursor", waitCursor);
